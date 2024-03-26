416e4cb1f9cb26f997dc905f3f1bf2279958c1b5
Stephen Ochieng <materiamystic@gmail.com>
2024-03-26 23:16:06 +0300

    Adds required functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, name) {
+  return arr.filter((item) => item.toLowerCase() === name.toLowerCase());
+}
+
+const drivers = ["Dave", "Torrido", "Jason", "Brooks", "Dan"];
+
+// console.log(fuzzyMatch(drivers, "d"));
+
+function fuzzyMatch(arr, name) {
+  return arr.filter((item) => item[0].toLowerCase() === name[0].toLowerCase());
+}
+
+function matchName(objArr, record) {
+  return objArr.filter((item) => item.name === record);
+}
